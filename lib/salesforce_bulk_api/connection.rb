28531b9472ba625b52aa25956c1cb419b1bfcfcc
Yatish Mehta <yatish27@users.noreply.github.com>
2024-10-24 22:32:19 -0700

    Refactored the ruby files

--- lib/salesforce_bulk_api/connection.rb
+++ lib/salesforce_bulk_api/connection.rb
@@ -1,96 +1,92 @@
 require "timeout"
+require "net/https"
 
 module SalesforceBulkApi
   class Connection
     include Concerns::Throttling
 
-    LOGIN_HOST = "login.salesforce.com"
+    LOGIN_HOST = "login.salesforce.com".freeze
+
+    attr_reader :session_id, :server_url, :instance, :instance_host
 
     def initialize(api_version, client)
       @client = client
       @api_version = api_version
       @path_prefix = "/services/async/#{@api_version}/"
+      @counters = Hash.new(0)
 
       login
     end
 
-    def login
-      client_type = @client.class.to_s
-      case client_type
-      when "Restforce::Data::Client"
-        @session_id = @client.options[:oauth_token]
-        @server_url = @client.options[:instance_url]
-      else
-        @session_id = @client.oauth_token
-        @server_url = @client.instance_url
-      end
-      @instance = parse_instance
-      @instance_host = "#{@instance}.salesforce.com"
-    end
-
     def post_xml(host, path, xml, headers)
       host ||= @instance_host
-      if host != LOGIN_HOST # Not login, need to add session id to header
-        headers["X-SFDC-Session"] = @session_id
-        path = "#{@path_prefix}#{path}"
-      end
-      i = 0
-      begin
-        count :post
-        throttle(http_method: :post, path: path)
-        https(host).post(path, xml, headers).body
-      rescue
-        i += 1
-        if i < 3
-          puts "Request fail #{i}: Retrying #{path}"
-          retry
-        else
-          puts "FATAL: Request to #{path} failed three times."
-          raise
-        end
-      end
+      headers["X-SFDC-Session"] = @session_id unless host == LOGIN_HOST
+      path = "#{@path_prefix}#{path}" unless host == LOGIN_HOST
+
+      perform_request(:post, host, path, xml, headers)
     end
 
     def get_request(host, path, headers)
       host ||= @instance_host
       path = "#{@path_prefix}#{path}"
-      if host != LOGIN_HOST # Not login, need to add session id to header
-        headers["X-SFDC-Session"] = @session_id
-      end
+      headers["X-SFDC-Session"] = @session_id unless host == LOGIN_HOST
 
-      count :get
-      throttle(http_method: :get, path: path)
-      https(host).get(path, headers).body
-    end
-
-    def https(host)
-      req = Net::HTTP.new(host, 443)
-      req.use_ssl = true
-      req.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      req
+      perform_request(:get, host, path, nil, headers)
     end
 
     def counters
       {
-        get: get_counters[:get],
-        post: get_counters[:post]
+        get: @counters[:get],
+        post: @counters[:post]
       }
     end
 
     private
 
-    def get_counters
-      @counters ||= Hash.new(0)
+    def login
+      client_type = @client.class.to_s
+      @session_id, @server_url = if client_type == "Restforce::Data::Client"
+        [@client.options[:oauth_token], @client.options[:instance_url]]
+      else
+        [@client.oauth_token, @client.instance_url]
+      end
+      @instance = parse_instance
+      @instance_host = "#{@instance}.salesforce.com"
+    end
+
+    def perform_request(method, host, path, body, headers)
+      retries = 0
+      begin
+        count(method)
+        throttle(http_method: method, path: path)
+        response = https(host).public_send(method, path, body, headers)
+        response.body
+      rescue => e
+        retries += 1
+        if retries < 3
+          puts "Request fail #{retries}: Retrying #{path}"
+          retry
+        else
+          puts "FATAL: Request to #{path} failed three times."
+          raise e
+        end
+      end
+    end
+
+    def https(host)
+      Net::HTTP.new(host, 443).tap do |http|
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      end
     end
 
     def count(http_method)
-      get_counters[http_method] += 1
+      @counters[http_method] += 1
     end
 
     def parse_instance
-      @instance = @server_url.match(/https:\/\/[a-z]{2}[0-9]{1,2}\./).to_s.gsub("https://", "").split(".")[0]
-      @instance = @server_url.split(".salesforce.com")[0].split("://")[1] if @instance.nil? || @instance.empty?
-      @instance
+      instance = @server_url.match(%r{https://([a-z]{2}[0-9]{1,2})\.})&.captures&.first
+      instance || @server_url.split(".salesforce.com").first.split("://").last
     end
   end
 end
