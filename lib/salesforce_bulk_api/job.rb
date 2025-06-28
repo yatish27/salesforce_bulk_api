5eb63f7449a4b909542041fa6ad63bbc64339794
Existing User <existing@example.com>
2025-06-28 15:26:44 -0700

    Revert "Refactored the ruby files"
    This reverts commit 28531b9472ba625b52aa25956c1cb419b1bfcfcc.

--- lib/salesforce_bulk_api/job.rb
+++ lib/salesforce_bulk_api/job.rb
@@ -1,12 +1,8 @@
-require "timeout"
-
 module SalesforceBulkApi
   class Job
     attr_reader :job_id
 
     class SalesforceException < StandardError; end
-
-    XML_HEADER = '<?xml version="1.0" encoding="utf-8" ?>'.freeze
 
     def initialize(args)
       @job_id = args[:job_id]
@@ -16,6 +12,7 @@ module SalesforceBulkApi
       @records = args[:records]
       @connection = args[:connection]
       @batch_ids = []
+      @XML_HEADER = '<?xml version="1.0" encoding="utf-8" ?>'
     end
 
     def create_job(batch_size, send_nulls, no_null_list)
@@ -23,121 +20,243 @@ module SalesforceBulkApi
       @send_nulls = send_nulls
       @no_null_list = no_null_list
 
-      xml = build_job_xml
-      response = post_xml("job", xml)
-      parse_job_response(response)
+      xml = "#{@XML_HEADER}<jobInfo xmlns=\"http://www.force.com/2009/06/asyncapi/dataload\">"
+      xml += "<operation>#{@operation}</operation>"
+      xml += "<object>#{@sobject}</object>"
+      # This only happens on upsert
+      if !@external_field.nil?
+        xml += "<externalIdFieldName>#{@external_field}</externalIdFieldName>"
+      end
+      xml += "<contentType>XML</contentType>"
+      xml += "</jobInfo>"
+
+      path = "job"
+      headers = Hash["Content-Type" => "application/xml; charset=utf-8"]
+
+      response = @connection.post_xml(nil, path, xml, headers)
+      response_parsed = XmlSimple.xml_in(response)
+
+      # response may contain an exception, so raise it
+      raise SalesforceException.new("#{response_parsed["exceptionMessage"][0]} (#{response_parsed["exceptionCode"][0]})") if response_parsed["exceptionCode"]
+
+      @job_id = response_parsed["id"][0]
     end
 
     def close_job
-      xml = build_close_job_xml
-      response = post_xml("job/#{@job_id}", xml)
+      xml = "#{@XML_HEADER}<jobInfo xmlns=\"http://www.force.com/2009/06/asyncapi/dataload\">"
+      xml += "<state>Closed</state>"
+      xml += "</jobInfo>"
+
+      path = "job/#{@job_id}"
+      headers = Hash["Content-Type" => "application/xml; charset=utf-8"]
+
+      response = @connection.post_xml(nil, path, xml, headers)
       XmlSimple.xml_in(response)
     end
 
     def add_query
-      response = post_xml("job/#{@job_id}/batch/", @records)
+      path = "job/#{@job_id}/batch/"
+      headers = Hash["Content-Type" => "application/xml; charset=UTF-8"]
+
+      response = @connection.post_xml(nil, path, @records, headers)
       response_parsed = XmlSimple.xml_in(response)
+
       @batch_ids << response_parsed["id"][0]
     end
 
     def add_batches
-      raise ArgumentError, "Records must be an array of hashes." unless @records.is_a?(Array)
+      raise "Records must be an array of hashes." unless @records.is_a? Array
+      keys = @records.each_with_object({}) { |pairs, h|
+        pairs.each { |k, v| (h[k] ||= []) << v }
+      }.keys
 
-      keys = @records.each_with_object({}) { |pairs, h| pairs.each { |k, v| (h[k] ||= []) << v } }.keys
-      batches = @records.each_slice(@batch_size).to_a
+      @records_dup = @records.clone
 
-      batches.each do |batch|
+      super_records = []
+      (@records_dup.size / @batch_size).to_i.times do
+        super_records << @records_dup.pop(@batch_size)
+      end
+      super_records << @records_dup unless @records_dup.empty?
+
+      super_records.each do |batch|
         @batch_ids << add_batch(keys, batch)
       end
     end
 
-    def get_job_result(return_result, timeout)
-      state = []
-      Timeout.timeout(timeout, JobTimeout) do
-        loop do
-          job_status = check_job_status
-          break unless job_closed_and_batches_completed?(job_status, state)
-          break if @batch_ids.empty?
+    def add_batch(keys, batch)
+      xml = "#{@XML_HEADER}<sObjects xmlns=\"http://www.force.com/2009/06/asyncapi/dataload\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"
+      batch.each do |r|
+        xml += create_sobject(keys, r)
+      end
+      xml += "</sObjects>"
+      path = "job/#{@job_id}/batch/"
+      headers = Hash["Content-Type" => "application/xml; charset=UTF-8"]
+      response = @connection.post_xml(nil, path, xml, headers)
+      response_parsed = XmlSimple.xml_in(response)
+      response_parsed["id"][0] if response_parsed["id"]
+    end
+
+    def build_sobject(data)
+      xml = "<sObject>"
+      data.keys.each do |k|
+        if k.is_a?(Hash)
+          xml += build_sobject(k)
+        elsif k.to_s.include? "."
+          relations = k.to_s.split(".")
+          parent = relations[0]
+          child = relations[1..-1].join(".")
+          xml += "<#{parent}>#{build_sobject({child => data[k]})}</#{parent}>"
+        elsif data[k] != :type
+          xml += "<#{k}>#{data[k]}</#{k}>"
         end
       end
-    rescue JobTimeout => e
-      handle_timeout(e)
-    ensure
-      process_batch_results(state) if return_result
+      xml += "</sObject>"
+    end
+
+    def build_relationship_sobject(key, value)
+      if key.to_s.include? "."
+        relations = key.to_s.split(".")
+        parent = relations[0]
+        child = relations[1..-1].join(".")
+        xml = "<#{parent}>"
+        xml += "<sObject>"
+        xml += build_relationship_sobject(child, value)
+        xml += "</sObject>"
+        xml += "</#{parent}>"
+      else
+        xml = "<#{key}>#{value}</#{key}>"
+      end
+    end
+
+    def create_sobject(keys, r)
+      sobject_xml = "<sObject>"
+      keys.each do |k|
+        if r[k].is_a?(Hash)
+          sobject_xml += "<#{k}>"
+          sobject_xml += build_sobject(r[k])
+          sobject_xml += "</#{k}>"
+        elsif k.to_s.include? "."
+          sobject_xml += build_relationship_sobject(k, r[k])
+        elsif !r[k].to_s.empty?
+          sobject_xml += "<#{k}>"
+          sobject_xml += if r[k].respond_to?(:encode)
+            r[k].encode(xml: :text)
+          elsif r[k].respond_to?(:iso8601) # timestamps
+            r[k].iso8601.to_s
+          else
+            r[k].to_s
+          end
+          sobject_xml += "</#{k}>"
+        elsif @send_nulls && !@no_null_list.include?(k) && r.key?(k)
+          sobject_xml += "<#{k} xsi:nil=\"true\"/>"
+        end
+      end
+      sobject_xml += "</sObject>"
+      sobject_xml
+    end
+
+    def check_job_status
+      path = "job/#{@job_id}"
+      headers = {}
+      response = @connection.get_request(nil, path, headers)
+
+      begin
+        response_parsed = XmlSimple.xml_in(response) if response
+        response_parsed
+      rescue => e
+        puts "Error parsing XML response for #{@job_id}"
+        puts e
+        puts e.backtrace
+      end
+    end
+
+    def check_batch_status(batch_id)
+      path = "job/#{@job_id}/batch/#{batch_id}"
+      headers = {}
+
+      response = @connection.get_request(nil, path, headers)
+
+      begin
+        response_parsed = XmlSimple.xml_in(response) if response
+        response_parsed
+      rescue => e
+        puts "Error parsing XML response for #{@job_id}, batch #{batch_id}"
+        puts e
+        puts e.backtrace
+      end
+    end
+
+    def get_job_result(return_result, timeout)
+      # timeout is in seconds
+      begin
+        state = []
+        Timeout.timeout(timeout, SalesforceBulkApi::JobTimeout) do
+          while true
+            job_status = check_job_status
+            if job_status && job_status["state"] && job_status["state"][0] == "Closed"
+              batch_statuses = {}
+
+              batches_ready = @batch_ids.all? do |batch_id|
+                batch_state = batch_statuses[batch_id] = check_batch_status(batch_id)
+                batch_state && batch_state["state"] && batch_state["state"][0] && !["Queued", "InProgress"].include?(batch_state["state"][0])
+              end
+
+              if batches_ready
+                @batch_ids.each do |batch_id|
+                  state.insert(0, batch_statuses[batch_id])
+                  @batch_ids.delete(batch_id)
+                end
+              end
+              break if @batch_ids.empty?
+            else
+              break
+            end
+          end
+        end
+      rescue SalesforceBulkApi::JobTimeout => e
+        puts 'Timeout waiting for Salesforce to process job batches #{@batch_ids} of job #{@job_id}.'
+        puts e
+        raise
+      end
+
+      state.each_with_index do |batch_state, i|
+        if batch_state["state"][0] == "Completed" && return_result == true
+          state[i].merge!({"response" => get_batch_result(batch_state["id"][0])})
+        end
+      end
       state
     end
 
-    private
+    def get_batch_result(batch_id)
+      path = "job/#{@job_id}/batch/#{batch_id}/result"
+      headers = Hash["Content-Type" => "application/xml; charset=UTF-8"]
 
-    def build_job_xml
-      xml = "#{XML_HEADER}<jobInfo xmlns=\"http://www.force.com/2009/06/asyncapi/dataload\">"
-      xml << "<operation>#{@operation}</operation>"
-      xml << "<object>#{@sobject}</object>"
-      xml << "<externalIdFieldName>#{@external_field}</externalIdFieldName>" if @external_field
-      xml << "<contentType>XML</contentType>"
-      xml << "</jobInfo>"
-    end
-
-    def build_close_job_xml
-      "#{XML_HEADER}<jobInfo xmlns=\"http://www.force.com/2009/06/asyncapi/dataload\"><state>Closed</state></jobInfo>"
-    end
-
-    def post_xml(path, xml)
-      headers = {"Content-Type" => "application/xml; charset=utf-8"}
-      @connection.post_xml(nil, path, xml, headers)
-    end
-
-    def parse_job_response(response)
+      response = @connection.get_request(nil, path, headers)
       response_parsed = XmlSimple.xml_in(response)
-      if response_parsed["exceptionCode"]
-        raise SalesforceException, "#{response_parsed["exceptionMessage"][0]} (#{response_parsed["exceptionCode"][0]})"
+      results = response_parsed["result"] unless @operation == "query"
+
+      if @operation == "query" # The query op requires us to do another request to get the results
+        result_id = response_parsed["result"][0]
+        path = "job/#{@job_id}/batch/#{batch_id}/result/#{result_id}"
+        headers = {}
+        headers = Hash["Content-Type" => "application/xml; charset=UTF-8"]
+        response = @connection.get_request(nil, path, headers)
+        response_parsed = XmlSimple.xml_in(response)
+        results = response_parsed["records"]
       end
-      @job_id = response_parsed["id"][0]
+      results
     end
 
-    def add_batch(keys, batch)
-      xml = "#{XML_HEADER}<sObjects xmlns=\"http://www.force.com/2009/06/asyncapi/dataload\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">"
-      batch.each { |r| xml << create_sobject(keys, r) }
-      xml << "</sObjects>"
+    def get_batch_records(batch_id)
+      path = "job/#{@job_id}/batch/#{batch_id}/request"
+      headers = Hash["Content-Type" => "application/xml; charset=UTF-8"]
 
-      response = post_xml("job/#{@job_id}/batch/", xml)
+      response = @connection.get_request(nil, path, headers)
       response_parsed = XmlSimple.xml_in(response)
-      response_parsed["id"]&.first
-    end
-
-    def job_closed_and_batches_completed?(job_status, state)
-      return false unless job_status && job_status["state"] && job_status["state"][0] == "Closed"
-
-      batch_statuses = {}
-      batches_ready = @batch_ids.all? do |batch_id|
-        batch_state = batch_statuses[batch_id] = check_batch_status(batch_id)
-        batch_state && batch_state["state"] && batch_state["state"][0] && !["Queued", "InProgress"].include?(batch_state["state"][0])
-      end
-
-      if batches_ready
-        @batch_ids.each do |batch_id|
-          state.unshift(batch_statuses[batch_id])
-          @batch_ids.delete(batch_id)
-        end
-      end
-
-      true
-    end
-
-    def handle_timeout(error)
-      puts "Timeout waiting for Salesforce to process job batches #{@batch_ids} of job #{@job_id}."
-      puts error
-      raise
-    end
-
-    def process_batch_results(state)
-      state.each_with_index do |batch_state, i|
-        if batch_state["state"][0] == "Completed"
-          state[i].merge!("response" => get_batch_result(batch_state["id"][0]))
-        end
-      end
+      response_parsed["sObject"]
     end
   end
 
-  class JobTimeout < StandardError; end
+  class JobTimeout < StandardError
+  end
 end
