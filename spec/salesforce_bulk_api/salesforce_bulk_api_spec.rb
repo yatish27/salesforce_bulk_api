f25302371d534371e8cc44e1aeb415a66315cba5
Existing User <existing@example.com>
2025-07-02 21:24:27 -0700

    chore: Update RuboCop configuration, gemspec dependencies, and CI workflow for improved linting and code quality

--- spec/salesforce_bulk_api/salesforce_bulk_api_spec.rb
+++ spec/salesforce_bulk_api/salesforce_bulk_api_spec.rb
@@ -3,8 +3,8 @@ require "yaml"
 require "restforce"
 
 describe SalesforceBulkApi do
-  before :each do
-    @sf_client = Restforce.new(
+  let(:sf_client) do
+    client = Restforce.new(
       username: ENV["SALESFORCE_USERNAME"],
       password: ENV["SALESFORCE_PASSWORD"],
       client_id: ENV["SALESFORCE_CLIENT_ID"],
@@ -12,62 +12,61 @@ describe SalesforceBulkApi do
       host: ENV["SALESFORCE_HOST"],
       security_token: ENV["SALESFORCE_SECURITY_TOKEN"]
     )
-
-    @sf_client.authenticate!
-
-    @account_id = ENV["SALESFORCE_TEST_ACCOUNT_ID"]
-
-    @api = SalesforceBulkApi::Api.new(@sf_client)
+    client.authenticate!
+    client
   end
+
+  let(:account_id) { ENV["SALESFORCE_TEST_ACCOUNT_ID"] }
+  let(:api) { SalesforceBulkApi::Api.new(sf_client) }
 
   describe "upsert" do
     context "when not passed get_result" do
       it "doesn't return the batches array" do
-        res = @api.upsert("Account", [{Id: @account_id, Website: "www.test.com"}], "Id")
-        res["batches"].should be_nil
+        res = api.upsert("Account", [{Id: account_id, Website: "www.test.com"}], "Id")
+        expect(res["batches"]).to be_nil
       end
     end
 
     context "when passed get_result = true" do
       it "returns the batches array" do
-        res = @api.upsert("Account", [{Id: @account_id, Website: "www.test.com"}], "Id", true)
-        res["batches"][0]["response"].is_a? Array
+        res = api.upsert("Account", [{Id: account_id, Website: "www.test.com"}], "Id", true)
+        expect(res["batches"][0]["response"]).to be_a Array
 
-        res["batches"][0]["response"][0]["id"][0].should start_with(@account_id)
-        res["batches"][0]["response"][0]["success"].should eq ["true"]
-        res["batches"][0]["response"][0]["created"].should eq ["false"]
+        expect(res["batches"][0]["response"][0]["id"][0]).to start_with(account_id)
+        expect(res["batches"][0]["response"][0]["success"]).to eq ["true"]
+        expect(res["batches"][0]["response"][0]["created"]).to eq ["false"]
       end
     end
 
     context "when passed send_nulls = true" do
       it "sets the nil and empty attributes to NULL" do
-        @api.update("Account", [{Id: @account_id, Website: "abc123", Phone: "5678"}], true)
-        res = @api.query("Account", "SELECT Website, Phone From Account WHERE Id = '#{@account_id}'")
-        res["batches"][0]["response"][0]["Website"][0].should eq "abc123"
-        res["batches"][0]["response"][0]["Phone"][0].should eq "5678"
-        res = @api.upsert("Account", [{Id: @account_id, Website: "", Phone: nil}], "Id", true, true)
-        res["batches"][0]["response"][0]["id"][0].should start_with(@account_id)
-        res["batches"][0]["response"][0]["success"].should eq ["true"]
-        res["batches"][0]["response"][0]["created"].should eq ["false"]
-        res = @api.query("Account", "SELECT Website, Phone From Account WHERE Id = '#{@account_id}'")
-        res["batches"][0]["response"][0]["Website"][0].should eq({"xsi:nil" => "true"})
-        res["batches"][0]["response"][0]["Phone"][0].should eq({"xsi:nil" => "true"})
+        api.update("Account", [{Id: account_id, Website: "abc123", Phone: "5678"}], true)
+        res = api.query("Account", "SELECT Website, Phone From Account WHERE Id = '#{account_id}'")
+        expect(res["batches"][0]["response"][0]["Website"][0]).to eq "abc123"
+        expect(res["batches"][0]["response"][0]["Phone"][0]).to eq "5678"
+        res = api.upsert("Account", [{Id: account_id, Website: "", Phone: nil}], "Id", true, true)
+        expect(res["batches"][0]["response"][0]["id"][0]).to start_with(account_id)
+        expect(res["batches"][0]["response"][0]["success"]).to eq ["true"]
+        expect(res["batches"][0]["response"][0]["created"]).to eq ["false"]
+        res = api.query("Account", "SELECT Website, Phone From Account WHERE Id = '#{account_id}'")
+        expect(res["batches"][0]["response"][0]["Website"][0]).to eq({"xsi:nil" => "true"})
+        expect(res["batches"][0]["response"][0]["Phone"][0]).to eq({"xsi:nil" => "true"})
       end
     end
 
     context "when passed send_nulls = true and an array of fields not to null" do
       it "sets the nil and empty attributes to NULL, except for those included in the list of fields to ignore" do
-        @api.update("Account", [{Id: @account_id, Website: "abc123", Phone: "5678"}], true)
-        res = @api.query("Account", "SELECT Website, Phone From Account WHERE Id = '#{@account_id}'")
-        res["batches"][0]["response"][0]["Website"][0].should eq "abc123"
-        res["batches"][0]["response"][0]["Phone"][0].should eq "5678"
-        res = @api.upsert("Account", [{Id: @account_id, Website: "", Phone: nil}], "Id", true, true, [:Website, :Phone])
-        res["batches"][0]["response"][0]["id"][0].should start_with(@account_id)
-        res["batches"][0]["response"][0]["success"].should eq ["true"]
-        res["batches"][0]["response"][0]["created"].should eq ["false"]
-        res = @api.query("Account", "SELECT Website, Phone From Account WHERE Id = '#{@account_id}'")
-        res["batches"][0]["response"][0]["Website"][0].should eq("abc123")
-        res["batches"][0]["response"][0]["Phone"][0].should eq("5678")
+        api.update("Account", [{Id: account_id, Website: "abc123", Phone: "5678"}], true)
+        res = api.query("Account", "SELECT Website, Phone From Account WHERE Id = '#{account_id}'")
+        expect(res["batches"][0]["response"][0]["Website"][0]).to eq "abc123"
+        expect(res["batches"][0]["response"][0]["Phone"][0]).to eq "5678"
+        res = api.upsert("Account", [{Id: account_id, Website: "", Phone: nil}], "Id", true, true, [:Website, :Phone])
+        expect(res["batches"][0]["response"][0]["id"][0]).to start_with(account_id)
+        expect(res["batches"][0]["response"][0]["success"]).to eq ["true"]
+        expect(res["batches"][0]["response"][0]["created"]).to eq ["false"]
+        res = api.query("Account", "SELECT Website, Phone From Account WHERE Id = '#{account_id}'")
+        expect(res["batches"][0]["response"][0]["Website"][0]).to eq("abc123")
+        expect(res["batches"][0]["response"][0]["Phone"][0]).to eq("5678")
       end
     end
   end
@@ -76,18 +75,18 @@ describe SalesforceBulkApi do
     context "when there is not an error" do
       context "when not passed get_result" do
         it "doesnt return the batches array" do
-          res = @api.update("Account", [{Id: @account_id, Website: "www.test.com"}])
-          res["batches"].should be_nil
+          res = api.update("Account", [{Id: account_id, Website: "www.test.com"}])
+          expect(res["batches"]).to be_nil
         end
       end
 
       context "when passed get_result = true" do
         it "returns the batches array" do
-          res = @api.update("Account", [{Id: @account_id, Website: "www.test.com"}], true)
-          res["batches"][0]["response"].is_a? Array
-          res["batches"][0]["response"][0]["id"][0].should start_with(@account_id)
-          res["batches"][0]["response"][0]["success"].should eq ["true"]
-          res["batches"][0]["response"][0]["created"].should eq ["false"]
+          res = api.update("Account", [{Id: account_id, Website: "www.test.com"}], true)
+          expect(res["batches"][0]["response"]).to be_a Array
+          expect(res["batches"][0]["response"][0]["id"][0]).to start_with(account_id)
+          expect(res["batches"][0]["response"][0]["success"]).to eq ["true"]
+          expect(res["batches"][0]["response"][0]["created"]).to eq ["false"]
         end
       end
     end
@@ -95,26 +94,26 @@ describe SalesforceBulkApi do
     context "when there is an error" do
       context "when not passed get_result" do
         it "doesn't return the results array" do
-          res = @api.update("Account", [{Id: @account_id, Website: "www.test.com"}, {Id: "abc123", Website: "www.test.com"}])
-          res["batches"].should be_nil
+          res = api.update("Account", [{Id: account_id, Website: "www.test.com"}, {Id: "abc123", Website: "www.test.com"}])
+          expect(res["batches"]).to be_nil
         end
       end
 
       context "when passed get_result = true with batches" do
         it "returns the results array" do
-          res = @api.update("Account", [{Id: @account_id, Website: "www.test.com"}, {Id: @account_id, Website: "www.test.com"}, {Id: @account_id, Website: "www.test.com"}, {Id: "abc123", Website: "www.test.com"}], true, false, [], 2)
+          res = api.update("Account", [{Id: account_id, Website: "www.test.com"}, {Id: account_id, Website: "www.test.com"}, {Id: account_id, Website: "www.test.com"}, {Id: "abc123", Website: "www.test.com"}], true, false, [], 2)
 
-          res["batches"][0]["response"][0]["id"][0].should start_with(@account_id)
-          res["batches"][0]["response"][0]["success"].should eq ["true"]
-          res["batches"][0]["response"][0]["created"].should eq ["false"]
-          res["batches"][0]["response"][1]["id"][0].should start_with(@account_id)
-          res["batches"][0]["response"][1]["success"].should eq ["true"]
-          res["batches"][0]["response"][1]["created"].should eq ["false"]
+          expect(res["batches"][0]["response"][0]["id"][0]).to start_with(account_id)
+          expect(res["batches"][0]["response"][0]["success"]).to eq ["true"]
+          expect(res["batches"][0]["response"][0]["created"]).to eq ["false"]
+          expect(res["batches"][0]["response"][1]["id"][0]).to start_with(account_id)
+          expect(res["batches"][0]["response"][1]["success"]).to eq ["true"]
+          expect(res["batches"][0]["response"][1]["created"]).to eq ["false"]
 
-          res["batches"][1]["response"][0]["id"][0].should start_with(@account_id)
-          res["batches"][1]["response"][0]["success"].should eq ["true"]
-          res["batches"][1]["response"][0]["created"].should eq ["false"]
-          res["batches"][1]["response"][1].should eq({"errors" => [{"fields" => ["Id"], "message" => ["Account ID: id value of incorrect type: abc123"], "statusCode" => ["MALFORMED_ID"]}], "success" => ["false"], "created" => ["false"]})
+          expect(res["batches"][1]["response"][0]["id"][0]).to start_with(account_id)
+          expect(res["batches"][1]["response"][0]["success"]).to eq ["true"]
+          expect(res["batches"][1]["response"][0]["created"]).to eq ["false"]
+          expect(res["batches"][1]["response"][1]).to eq({"errors" => [{"fields" => ["Id"], "message" => ["Account ID: id value of incorrect type: abc123"], "statusCode" => ["MALFORMED_ID"]}], "success" => ["false"], "created" => ["false"]})
         end
       end
     end
@@ -131,9 +130,9 @@ describe SalesforceBulkApi do
   describe "query" do
     context "when there are results" do
       it "returns the query results" do
-        res = @api.query("Account", "SELECT id, Name From Account WHERE Name LIKE 'Test%'")
-        res["batches"][0]["response"].length.should > 1
-        res["batches"][0]["response"][0]["Id"].should_not be_nil
+        res = api.query("Account", "SELECT id, Name From Account WHERE Name LIKE 'Test%'")
+        expect(res["batches"][0]["response"].length).to be > 1
+        expect(res["batches"][0]["response"][0]["Id"]).not_to be_nil
       end
 
       context "and there are multiple batches" do
@@ -144,15 +143,15 @@ describe SalesforceBulkApi do
 
     context "when there are no results" do
       it "returns nil" do
-        res = @api.query("Account", "SELECT id From Account WHERE Name = 'ABC'")
-        res["batches"][0]["response"].should eq nil
+        res = api.query("Account", "SELECT id From Account WHERE Name = 'ABC'")
+        expect(res["batches"][0]["response"]).to be_nil
       end
     end
 
     context "when there is an error" do
       it "returns nil" do
-        res = @api.query("Account", "SELECT id From Account WHERE Name = ''ABC'")
-        res["batches"][0]["response"].should eq nil
+        res = api.query("Account", "SELECT id From Account WHERE Name = ''ABC'")
+        expect(res["batches"][0]["response"]).to be_nil
       end
     end
   end
